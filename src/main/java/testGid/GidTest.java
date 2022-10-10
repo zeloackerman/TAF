206bf75c6876750a8c07b3432b65d5ae33ce66d9
Gulbara <gulbarazhanybai@gmail.com>
2022-10-10 21:48:59 +0600

    Third

--- src/main/java/testGid/GidTest.java
+++ src/main/java/testGid/GidTest.java
@@ -4,5 +4,6 @@ public class GidTest {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("test");
     }
 }
---
+++ src/main/java/testGid/Test2.java
@@ -0,0 +1,4 @@
+package testGid;
+
+public class Test2 {
+}
